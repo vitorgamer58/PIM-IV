15c5294b3dfa97388af7a8405f8526352c19a69c
Bruno Paulino <149417295+brunnops@users.noreply.github.com>
2023-11-01 18:32:58 -0300

    novos usuários e espaçamentos
    Iniciando a escrita da tela de cadastro de novos usuários e alguns ajustes visuais em espaçamentos e títulos.

--- main.c
+++ main.c
@@ -5,6 +5,26 @@
 #include "types.h"
 #include "userRepository.h"
 
+void cadastrarUsuario()
+{
+    char nomeDeUsuario[50], senha[50];
+
+    system("cls");
+    printf("------------------------------------------------\n");
+    printf("          CADASTRO DE NOVOS USUARIOS\n");
+    printf("------------------------------------------------\n");
+    printf("\n");
+    printf("Digite o nome do novo usuario: ");
+    scanf("%s", nomeDeUsuario);
+    printf("Agora digite a senha: ");
+    scanf("%s", senha);
+    printf("\n");
+    printf("----- NOVO USUARIO CADASTRADO COM SUCESSO! -----\n");
+    printf("\n");
+    system ("pause");
+
+
+}
 void renderizaMenuEDireciona()
 {
     int escolha;
@@ -12,19 +32,23 @@ void renderizaMenuEDireciona()
     do
     {
         system("cls");
-        printf("Menu:\n");
+        printf("------------------------------------------------\n");
+        printf("                MENU PRINCIPAL:\n");
+        printf("------------------------------------------------\n");
+        printf("\n");
         printf("1. Cadastrar novo usuario\n");
         printf("2. Cadastrar industria\n");
         printf("3. Exibir dados da industria\n");
         printf("4. Exibir relatorios\n");
         printf("5. Sair\n");
+        printf("\n");
         printf("Escolha uma opcao: ");
         scanf("%d", &escolha);
 
         switch(escolha)
         {
         case 1:
-            break;
+            cadastrarUsuario();
         case 2:
             break;
         case 3:
@@ -54,15 +78,16 @@ void logar(char* nomeDeUsuario, char* senha)
 {
     Usuario usuario;
 
-    printf("Digite seu usuario:");
+    printf("Digite seu usuario: ");
     scanf("%s", nomeDeUsuario);
 
-    printf("Digite sua senha:");
+    printf("Digite sua senha: ");
     scanf("%s", senha);
 
     usuario = buscarUsuario(nomeDeUsuario);
 
-    if(!usuario.isValid) {
+    if(!usuario.isValid)
+    {
         printf("Usuario nao encontrado\n");
         return;
     }
@@ -86,8 +111,10 @@ int main()
 {
     char nomeDeUsuario[50], senha[50];
 
-    printf("Bem vindo ao sistema\n");
-
+    printf("------------------------------------------------\n");
+    printf("              BEM-VINDO AO SISTEMA!\n");
+    printf("------------------------------------------------\n");
+    printf("\n");
     logar(nomeDeUsuario, senha);
 
     return 0;
