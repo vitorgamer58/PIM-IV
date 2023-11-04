009d03753864f8d94966e04ec4387019b084e608
Bruno Paulino <149417295+brunnops@users.noreply.github.com>
2023-11-03 23:06:14 -0300

    base do cadastro de indústrias, atualizações de telas e idioma em português
    foi feita a base de variáveis, printf, scanf e telas do cadastro de indústrias, outras telas do código também foram atualizadas e foi acrescentado um recurso da biblioteca locale.h (função setlocale) para os textos serem visualizados em português (com acentos, cedilha).

--- main.c
+++ main.c
@@ -2,19 +2,21 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <windows.h>
+#include <locale.h>
 #include "types.h"
 #include "userRepository.h"
 
-void cadastrarUsuario()
+void cadastrarUsuario() // TELA DE CADASTRO DE USUÁRIOS
 {
     char nomeDeUsuario[50], senha[50];
 
     system("cls");
-    printf("------------------------------------------------\n");
-    printf("          CADASTRO DE NOVOS USUARIOS\n");
-    printf("------------------------------------------------\n");
+    printf("------------------------------------------------------\n");
+    printf("              CADASTRO DE NOVOS USUÁRIOS\n");
+    printf("------------------------------------------------------\n");
     printf("\n");
-    printf("Digite o nome do novo usuario: ");
+    printf("Digite o nome do novo usuário: ");
     scanf("%s", nomeDeUsuario);
     printf("Agora digite a senha: ");
     scanf("%s", senha);
@@ -23,33 +25,119 @@ void cadastrarUsuario()
 
     if (usuarioFoiCadastrado)
     {
-        printf("----- NOVO USUARIO CADASTRADO COM SUCESSO! -----\n");
+        printf("-------- NOVO USUÁRIO CADASTRADO COM SUCESSO! --------\n");
     }
+
     printf("\n");
     system ("pause");
+    renderizaMenuEDireciona();
+
+}
+
+void cadastrarIndustria() // TELA DE CADASTRO DE INDUSTRIAS
+{
+    char nomeDoResp[50], enderecoDoResp[100], nomeDaEmpresa[50], razaoSocial[50], nomeFantasia[50];
+    char enderecoDaEmpresa[100], emailEmpresa[30];
+    int cpf[12], cnpj[15], telDoResp[15], telDaEmpresa[15], aberturaEmpresa[11];
+
+    system("cls");
+    printf("------------------------------------------------------\n");
+    printf("               CADASTRO DE INDÚSTRIAS\n");
+    printf("------------------------------------------------------\n");
+    printf("  Iniciando com os dados do responsável pela empresa.\n");
+    printf("\n");
+
+    // FORMULÁRIO COM BASE NAS INFORMAÇÕES SUGERIDAS DO PIM
+
+    printf("- Qual o nome do responsável pela empresa? ");
+    scanf("%s", nomeDoResp);
+    printf("\n");
+
+    printf("- Qual o CPF (apenas números) do responsável pela empresa? ");
+    scanf("%d", cpf);
+    printf("\n");
+
+    printf("---> Qual o endereço do responsável pela empresa?\n");
+    printf("SIGA O PADRÃO: RUA, NÚMERO, BAIRRO, CIDADE, ESTADO e CEP\n");
+    printf("\n");
+    printf("- Digite aqui: ");
+    scanf("%s", enderecoDoResp);
+    printf("\n");
+
+    printf("- Qual o número de telefone/celular do responsável pela empresa? ");
+    scanf("%d", &telDoResp);
+    printf(" \n");
 
 
+    printf("------------------------------------------------------\n");
+    printf("                DADOS CADASTRADOS!\n");
+    printf("------------------------------------------------------\n");
+    printf("       Agora vamos cadastrar os dados da empresa.\n");
+    printf("\n");
+
+    printf("- Qual o nome empresa? ");
+    scanf("%s", nomeDaEmpresa);
+    printf(" \n");
+
+    printf("- Qual o CNPJ (apenas números) da empresa? ");
+    scanf("%d", &cnpj);
+    printf(" \n");
+
+    printf("- Qual a razão social da empresa? ");
+    scanf("%s", razaoSocial);
+    printf(" \n");
+
+    printf("- Qual o nome fantasia da empresa? ");
+    scanf("%s", nomeFantasia);
+    printf(" \n");
+
+    printf("- Qual o número de telefone da empresa? ");
+    scanf("%d", &telDaEmpresa);
+    printf(" \n");
+
+    printf("---> Qual o endereço da empresa?\n");
+    printf("SIGA O PADRÃO: RUA, NÚMERO, BAIRRO, CIDADE, ESTADO e CEP\n");
+    printf("\n");
+    printf("- Digite aqui: ");
+    scanf("%s", enderecoDaEmpresa);
+    printf(" \n");
+
+    printf("- Qual o e-mail da empresa? ");
+    scanf("%s", emailEmpresa);
+    printf(" \n");
+
+    printf("- Qual a data de abertura da empresa? ");
+    scanf("%d", &aberturaEmpresa);
+    printf("\n");
+
+    printf("------------------------------------------------------\n");
+    printf("          INDÚSTRIA CADASTRADA COM SUCESSO!\n");
+    printf("------------------------------------------------------\n");
+
+    printf("\n");
+    system("pause");
 
 
 }
-void renderizaMenuEDireciona()
+
+void renderizaMenuEDireciona() // TELA DE MENU E DIRECIONAMENTO
 {
     int escolha;
 
     do
     {
         system("cls");
-        printf("------------------------------------------------\n");
-        printf("                MENU PRINCIPAL:\n");
-        printf("------------------------------------------------\n");
+        printf("------------------------------------------------------\n");
+        printf("                    MENU PRINCIPAL:\n");
+        printf("------------------------------------------------------\n");
         printf("\n");
-        printf("1. Cadastrar novo usuario\n");
-        printf("2. Cadastrar industria\n");
-        printf("3. Exibir dados da industria\n");
-        printf("4. Exibir relatorios\n");
+        printf("1. Cadastrar novo usuário\n");
+        printf("2. Cadastrar indústria\n");
+        printf("3. Exibir dados da indústria\n");
+        printf("4. Exibir relatórios\n");
         printf("5. Sair\n");
         printf("\n");
-        printf("Escolha uma opcao: ");
+        printf("Escolha uma opção: ");
         scanf("%d", &escolha);
 
         switch(escolha)
@@ -57,7 +145,7 @@ void renderizaMenuEDireciona()
         case 1:
             cadastrarUsuario();
         case 2:
-            break;
+            cadastrarIndustria();
         case 3:
             break;
         case 4:
@@ -65,7 +153,7 @@ void renderizaMenuEDireciona()
         case 5:
             return;
         default:
-            printf("Opcao invalida! Tente novamente.\n");
+            printf("Opção inválida! Tente novamente.\n");
         }
     }
 
@@ -82,11 +170,11 @@ bool checarSenha(const char* senhaDoUsuario, const char* senhaDigitada)
     return false;
 }
 
-void logar(char* nomeDeUsuario, char* senha)
+void logar(char* nomeDeUsuario, char* senha) // FUNÇÃO E TELAS DE LOGIN
 {
     Usuario usuario;
 
-    printf("Digite seu usuario: ");
+    printf("Digite seu usuário: ");
     scanf("%s", nomeDeUsuario);
 
     printf("Digite sua senha: ");
@@ -97,9 +185,9 @@ void logar(char* nomeDeUsuario, char* senha)
     if(!usuario.isValid)
     {
         printf("\n");
-        printf("--------- ERRO: Usuario nao encontrado ---------\n");
+        printf("-----------  ERRO: usuário não encontrado  -----------\n");
         printf("\n");
-        printf("                TENTE NOVAMENTE\n");
+        printf("                   TENTE NOVAMENTE\n");
         printf("\n");
         system("pause");
         system("cls");
@@ -112,14 +200,23 @@ void logar(char* nomeDeUsuario, char* senha)
     if(checarSenha(usuario.senha, senha))
     {
         printf("\n");
-        printf("Logado com sucesso!!");
+        printf("--------------   LOGADO COM SUCESSO!!   --------------");
+        Sleep(1200);
+        system ("cls");
+
+        // TELA DE BOAS VINDAS
+
+        printf("------------------------------------------------------\n");
+        printf("           BEM-VINDOS AO SISTEMA DA STARTUP!\n");
+        printf("------------------------------------------------------\n");
+        Sleep(2200);
         renderizaMenuEDireciona();
     }
 
     else
     {
         printf("\n");
-        printf("Senha invalida! Tente novamente!\n");
+        printf("Senha inválida! Tente novamente!\n");
         printf("\n");
         memset(nomeDeUsuario, 0, sizeof(nomeDeUsuario));
         memset(senha, 0, sizeof(senha));
@@ -127,13 +224,15 @@ void logar(char* nomeDeUsuario, char* senha)
     }
 }
 
-int main()
+int main() // TELA INCIAL
 {
     char nomeDeUsuario[50], senha[50];
 
-    printf("------------------------------------------------\n");
-    printf("              BEM-VINDO AO SISTEMA!\n");
-    printf("------------------------------------------------\n");
+    setlocale(LC_ALL, "Portuguese");
+    printf("------------------------------------------------------\n");
+    printf("          | STARTUP DE SOLUÇÕES AMBIENTAIS |\n");
+    printf("------------------------------------------------------\n");
+    printf(" Olá! Efetue login para ter acesso ao nosso sistema.\n");
     printf("\n");
     logar(nomeDeUsuario, senha);
 
