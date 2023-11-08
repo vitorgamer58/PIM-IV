9c4ddd1cf3f2fc3334b7f0bf698649ad376f25b5
Vitor Araujo <vitorgamer58@outlook.com>
2023-11-07 21:58:04 -0300

    Remove função de empresa do usuarioRepository

--- usuarioRepository.c
+++ usuarioRepository.c
@@ -51,34 +51,3 @@ bool inserirUsuario(const char* nomeDeUsuario, const char* senha)
     return true;
 }
 
-
-int exibirDadosIndustria() // Esboço da função de exibição de dados
-{
-    // Nome do arquivo a ser lido.
-    char nomeArquivo[] = "usuarios.txt";
-
-    // Abre o arquivo para leitura.
-    FILE *arquivo = fopen(nomeArquivo, "r");
-
-    // Verifica se o arquivo foi aberto com sucesso.
-    if (arquivo == NULL)
-    {
-        printf("Erro ao abrir o arquivo.\n");
-        return 1;
-    }
-
-    // Lê e exibe os dados do arquivo linha por linha.
-    char linha[100]; // Uma linha pode ter até 100 caracteres, ajuste conforme suas necessidades.
-
-    while (fgets(linha, sizeof(linha), arquivo) != NULL)
-    {
-        printf("%s", linha);
-    }
-
-        system("pause");
-    // Fecha o arquivo.
-    fclose(arquivo);
-
-    return 0;
-}
-
