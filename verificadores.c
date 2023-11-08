f5ccb82fe6112d86e798b2aa02cdd2b7fad4b85a
Vitor Araujo <vitorgamer58@outlook.com>
2023-11-07 21:57:15 -0300

    Correções gerais nos verificadores

--- verificadores.c
+++ verificadores.c
@@ -4,18 +4,17 @@
 
 void verificadorTelefone()
  {
-
     setlocale(LC_ALL, "Portuguese");
     char telefone[16]; // Buffer para armazenar o número de telefone (incluindo hífens)
 
     printf("---> Qual o número do celular do responsável pela empresa?\n");
     printf("Responda no formato XX-XX-9XXXXXXXX\n");
     printf("Os quatro primeiros dígitos referenciam o código do país e DDD.\n");
-    printf("Digite aqui: ")
+    printf("Digite aqui: ");
     scanf("%15s", telefone); // Ler até 15 caracteres (13 dígitos + 2 hífens)
 
     // Verificar se a entrada tem o tamanho correto e possui um hífen na posição correta
-    if (strlen(telefone) == 15 && telefone[2] == '-' && telefone[5] == '-' && [6] == '9') {
+    if (strlen(telefone) == 15 && telefone[2] == '-' && telefone[5] == '-' && telefone[6] == '9') {
         printf("Número de telefone válido: %s\n", telefone);
         // Agora você pode prosseguir com o armazenamento ou processamento do número de telefone.
     }
@@ -36,7 +35,7 @@ void verificadorCPF()
     setlocale(LC_ALL, "Portuguese");
     char cpf[15]; // Buffer para armazenar o cpf (incluindo pontos e hífen)
 
-    printf("---> Qual o CPF do responsável pela empresa?\n);
+    printf("---> Qual o CPF do responsável pela empresa?\n");
     printf("Preencha com pontos e hífen. Deve estar no formato XXX.XXX.XXX-XX\n");
     printf("Digite aqui: ");
     scanf("%14s", cpf); // Ler até 14 caracteres (11 números + 2 pontos + 1 hífen)
@@ -54,6 +53,6 @@ void verificadorCPF()
         verificadorCPF();
     }
 
-    return 0;
+    return;
 }
 
