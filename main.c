bc40e64131997958b73778c02d02fdf9004c7dea
Bruno Paulino <149417295+brunnops@users.noreply.github.com>
2023-11-09 10:47:18 -0300

    Pequenos ajustes em comandos visuais e funcionais
    Ajustes em alguns \n, em tela do "tente novamente" do menu principal, no switch-case e mais ou outro ajuste pequeno.

--- main.c
+++ main.c
@@ -15,22 +15,21 @@ void cadastrarUsuario() // TELA DE CADASTRO DE USUÁRIOS
     system("cls");
     printf("------------------------------------------------------\n");
     printf("              CADASTRO DE NOVOS USUÁRIOS\n");
-    printf("------------------------------------------------------\n");
-    printf("\n");
+    printf("------------------------------------------------------\n\n");
+
     printf("Digite o nome do novo usuário: ");
     scanf("%s", nomeDeUsuario);
     printf("Agora digite a senha: ");
     scanf("%s", senha);
-    printf("\n");
+
     bool usuarioFoiCadastrado=inserirUsuario(nomeDeUsuario, senha);
 
     if (usuarioFoiCadastrado)
     {
-        printf("-------- NOVO USUÁRIO CADASTRADO COM SUCESSO! --------\n");
+        printf("\n-------- NOVO USUÁRIO CADASTRADO COM SUCESSO! --------\n");
     }
 
-    printf("\n");
-    system ("pause");
+    Sleep(1500);
     renderizaMenuEDireciona();
 }
 
@@ -42,67 +41,54 @@ void cadastrarIndustria() // TELA DE CADASTRO DE INDUSTRIAS
     printf("------------------------------------------------------\n");
     printf("               CADASTRO DE INDÚSTRIAS\n");
     printf("------------------------------------------------------\n");
-    printf("  Iniciando com os dados do responsável pela empresa.\n");
-    printf("\n");
     getchar();
 
     // FORMULÁRIO COM BASE NAS INFORMAÇÕES SUGERIDAS DO PIM
 
-    printf("- Qual o nome do responsável pela empresa? ");
+    printf("\n- Qual o nome do responsável pela empresa? ");
     fgets(empresa.nomeDoResponsavel, sizeof(empresa.nomeDoResponsavel), stdin);
     empresa.nomeDoResponsavel[strcspn(empresa.nomeDoResponsavel, "\n")] = 0; // Remove o '\n'
-    printf("\n");
 
-    printf("- Qual o documento do responsável pela empresa? ");
+    printf("\n- Qual o documento do responsável pela empresa? ");
     fgets(empresa.documentoDoResponsavel, sizeof(empresa.documentoDoResponsavel), stdin);
     empresa.documentoDoResponsavel[strcspn(empresa.documentoDoResponsavel, "\n")] = 0;
-    printf("\n");
 
-    printf("- Qual a razão social da empresa? ");
+    printf("\n- Qual a razão social da empresa? ");
     fgets(empresa.razaoSocial, sizeof(empresa.razaoSocial), stdin);
     empresa.razaoSocial[strcspn(empresa.razaoSocial, "\n")] = 0;
-    printf("\n");
 
-    printf("- Qual o nome fantasia da empresa? ");
+    printf("\n- Qual o nome fantasia da empresa? ");
     fgets(empresa.nomeFantasia, sizeof(empresa.nomeFantasia), stdin);
     empresa.nomeFantasia[strcspn(empresa.nomeFantasia, "\n")] = 0;
-    printf("\n");
 
-    printf("- Qual o CNPJ (apenas números) da empresa? ");
+    printf("\n- Qual o CNPJ (apenas números) da empresa? ");
     fgets(empresa.cnpj, sizeof(empresa.cnpj), stdin);
     empresa.cnpj[strcspn(empresa.cnpj, "\n")] = 0;
-    printf("\n");
     getchar();
 
-    printf("- Qual o número de telefone da empresa? ");
+    printf("\n- Qual o número de telefone da empresa? ");
     fgets(empresa.telefoneDaEmpresa, sizeof(empresa.telefoneDaEmpresa), stdin);
     empresa.telefoneDaEmpresa[strcspn(empresa.telefoneDaEmpresa, "\n")] = 0;
-    printf("\n");
 
-    printf("---> Qual o endereço da empresa?\n");
-    printf("SIGA O PADRÃO: RUA, NÚMERO, BAIRRO, CIDADE, ESTADO e CEP\n");
-    printf("\n");
-    printf("- Digite aqui: ");
-    getchar();
+    printf("\n- Padrão para o preenchimento da próxima pergunta:\n");
+    printf("  > RUA, NÚMERO, BAIRRO, CIDADE, ESTADO e CEP <  \n");
+    printf("- Qual o endereço da empresa? ");
     fgets(empresa.enderecoDaEmpresa, sizeof(empresa.enderecoDaEmpresa), stdin);
-    empresa.enderecoDaEmpresa[strcspn(empresa.enderecoDaEmpresa, "\n")] = 0;
-    printf("\n");
+    empresa.enderecoDaEmpresa[strcspn(empresa.enderecoDaEmpresa, "\n\r")] = 0;
 
-    printf("- Qual o e-mail da empresa? ");
+    printf("\n- Qual o e-mail da empresa? ");
     fgets(empresa.emailDaEmpresa, sizeof(empresa.emailDaEmpresa), stdin);
-    empresa.emailDaEmpresa[strcspn(empresa.emailDaEmpresa, "\n")] = 0;
-    printf("\n");
+    empresa.emailDaEmpresa[strcspn(empresa.emailDaEmpresa, "\n\r")] = 0;
 
     // Aqui você deverá ter a função inserirEmpresa() definida em outra parte do seu código
     inserirEmpresa(empresa);
 
-    printf("------------------------------------------------------\n");
+    printf("\n------------------------------------------------------\n");
     printf("          INDÚSTRIA CADASTRADA COM SUCESSO!\n");
     printf("------------------------------------------------------\n");
 
-    printf("\n");
-    Sleep(1200); // Pausa o programa por 1200 milissegundos
-    // Aqui você deverá ter a função renderizaMenuEDireciona() definida em outra parte do seu código
+    Sleep(1500); /* Pausa o programa por 1.5 segundos;
+    Aqui você deverá ter a função renderizaMenuEDireciona() definida em outra parte do seu código */
     renderizaMenuEDireciona();
 }
 
@@ -115,33 +101,38 @@ void renderizaMenuEDireciona() // TELA DE MENU E DIRECIONAMENTO
         system("cls");
         printf("------------------------------------------------------\n");
         printf("                    MENU PRINCIPAL:\n");
-        printf("------------------------------------------------------\n");
-        printf("\n");
+        printf("------------------------------------------------------\n\n");
+
         printf("1. Cadastrar novo usuário\n");
         printf("2. Cadastrar indústria\n");
         printf("3. Exibir dados da indústria\n");
         printf("4. Exibir relatórios\n");
         printf("5. Sair\n");
-        printf("\n");
-        printf("Escolha uma opção: ");
+
+        printf("\nEscolha uma opção: ");
         scanf("%d", &escolha);
 
         switch(escolha)
         {
         case 1:
             cadastrarUsuario();
+            break;
         case 2:
             cadastrarIndustria();
+            break;
         case 3:
             break;
         case 4:
             break;
         case 5:
-            return;
+            printf("\n-------------------------------> Saindo do programa...\n");
+            exit(0);
+            break;
         default:
-            printf("\n");
-            printf("---------  Opção inválida! Tente novamente.  ---------\n");
-            printf("\n");
+            printf("\n---------  Opção inválida! Tente novamente.  ---------\n\n");
+            system("pause");
+            break;
+
         }
     }
     while(escolha != 5);
@@ -171,22 +162,19 @@ void logar(char* nomeDeUsuario, char* senha) // FUNÇÃO E TELAS DE LOGIN
 
     if(!usuario.isValid)
     {
-        printf("\n");
-        printf("-----------  ERRO: usuário não encontrado  -----------\n");
-        printf("\n");
-        printf("                   TENTE NOVAMENTE\n");
-        printf("\n");
+        printf("\n-----------  ERRO: usuário não encontrado  -----------\n\n");
+        printf("                   TENTE NOVAMENTE\n\n");
+
         system("pause");
         system("cls");
-        logar(nomeDeUsuario, senha);
+        main();
 
         return;
     }
 
     if(checarSenha(usuario.senha, senha))
     {
-        printf("\n");
-        printf("--------------   LOGADO COM SUCESSO!!   --------------");
+        printf("\n--------------   LOGADO COM SUCESSO!!   --------------");
         Sleep(1200);
         system ("cls");
 
@@ -202,8 +190,8 @@ void logar(char* nomeDeUsuario, char* senha) // FUNÇÃO E TELAS DE LOGIN
     else
     {
         printf("\n");
-        printf("Senha inválida! Tente novamente!\n");
-        printf("\n");
+        printf("Senha inválida! Tente novamente!\n\n");
+
         memset(nomeDeUsuario, 0, sizeof(nomeDeUsuario));
         memset(senha, 0, sizeof(senha));
         logar(nomeDeUsuario, senha);
@@ -218,8 +206,8 @@ int main() // TELA INCIAL
     printf("------------------------------------------------------\n");
     printf("          | STARTUP DE SOLUÇÕES AMBIENTAIS |\n");
     printf("------------------------------------------------------\n");
-    printf(" Olá! Efetue login para ter acesso ao nosso sistema.\n");
-    printf("\n");
+    printf(" Olá! Efetue login para ter acesso ao nosso sistema.\n\n");
+
     logar(nomeDeUsuario, senha);
 
     return 0;
