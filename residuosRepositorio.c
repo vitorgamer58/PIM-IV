e2b97a8579902ec4ff9164f3909359edc3cbfced
Vitor Araujo <vitorgamer58@outlook.com>
2023-11-17 00:15:26 -0300

    Adiciona buscarResiduoPorEmpresa

--- residuosRepositorio.c
+++ residuosRepositorio.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stddef.h>
+
 #include "types.h"
 #include "fileHelper.h"
+#include "listas/listas.h"
 
 bool inserirResiduo(Residuo residuo)
 {
@@ -28,4 +31,40 @@ bool inserirResiduo(Residuo residuo)
     fclose(ArquivoResiduos);
 
     return true;
+}
+
+ListaDeResiduos* buscarResiduoPorEmpresa(const char* cnpj)
+{
+    FILE *ArquivoResiduos = abrirOuCriarArquivo("Residuos.txt");
+    Residuo residuo;
+    ListaDeResiduos *listaDeResiduos = iniciaListaDeResiduos();
+
+    if(ArquivoResiduos == NULL)
+    {
+        return NULL;
+    }
+
+    while(fscanf(ArquivoResiduos, "%14[^;];%d", residuo.cnpj, &residuo.toneladas) != EOF)
+    {
+        residuo.cnpj[14] = '\0'; // Garantindo que a string seja terminada corretamente
+        printf("%s\n", cnpj);
+        printf("%s\n", residuo.cnpj);
+        printf("%d\n\n", strcmp(cnpj, residuo.cnpj));
+
+        size_t length_cnpj = strlen(cnpj);
+        size_t length_residuo_cnpj = strlen(residuo.cnpj);
+
+        printf("Comprimento de cnpj: %zu\n", length_cnpj);
+        printf("Comprimento de residuo.cnpj: %zu\n", length_residuo_cnpj);
+
+
+        if(strcmp(cnpj, residuo.cnpj) == 0)
+        {
+            listaDeResiduos = insereNaListaDeResiduos(listaDeResiduos, residuo);
+        }
+    }
+
+    fclose(ArquivoResiduos);
+
+    return listaDeResiduos;
 }
--- residuosRepositorio.h
+++ residuosRepositorio.h
@@ -1,6 +1,10 @@
 #ifndef RESIDUOSREPOSITORIO_H_INCLUDED
 #define RESIDUOSREPOSITORIO_H_INCLUDED
 
+#include "types.h"
+#include "listas/listas.h"
+
 bool inserirResiduo(Residuo residuo);
+ListaDeResiduos* buscarResiduoPorEmpresa(const char* cnpj);
 
 #endif // RESIDUOSREPOSITORIO_H_INCLUDED
