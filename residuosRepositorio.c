4e57c2f314af3e7c922e949b9bd90915df2a4aef
Vitor Araujo <vitorgamer58@outlook.com>
2023-11-18 17:16:58 -0300

    Altera estratégia de leitura do arquivo para usar fgets

--- residuosRepositorio.c
+++ residuosRepositorio.c
@@ -38,25 +38,17 @@ ListaDeResiduos* buscarResiduoPorEmpresa(const char* cnpj)
     FILE *ArquivoResiduos = abrirOuCriarArquivo("Residuos.txt");
     Residuo residuo;
     ListaDeResiduos *listaDeResiduos = iniciaListaDeResiduos();
+    char linha[1000];
 
     if(ArquivoResiduos == NULL)
     {
         return NULL;
     }
 
-    while(fscanf(ArquivoResiduos, "%14[^;];%d", residuo.cnpj, &residuo.toneladas) != EOF)
+    while(fgets(linha, sizeof(linha), ArquivoResiduos) != NULL)
     {
-        residuo.cnpj[14] = '\0'; // Garantindo que a string seja terminada corretamente
-        printf("%s\n", cnpj);
-        printf("%s\n", residuo.cnpj);
-        printf("%d\n\n", strcmp(cnpj, residuo.cnpj));
-
-        size_t length_cnpj = strlen(cnpj);
-        size_t length_residuo_cnpj = strlen(residuo.cnpj);
-
-        printf("Comprimento de cnpj: %zu\n", length_cnpj);
-        printf("Comprimento de residuo.cnpj: %zu\n", length_residuo_cnpj);
-
+        sscanf(linha, "%15[^;];%d", residuo.cnpj, &residuo.toneladas);
+        residuo.cnpj[15] = '\0';
 
         if(strcmp(cnpj, residuo.cnpj) == 0)
         {
@@ -68,3 +60,4 @@ ListaDeResiduos* buscarResiduoPorEmpresa(const char* cnpj)
 
     return listaDeResiduos;
 }
+
